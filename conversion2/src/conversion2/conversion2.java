8a10383f422fa052ab4595a7c57bbad2febb4d30
YooHanYeol <zerg1523@daum.net>
2019-05-20 01:30:44 +0900

    jar파일 변환

---
+++ conversion2/src/conversion2/conversion2.java
@@ -0,0 +1,23 @@
+package conversion2;
+import java.io.*;
+class conversion2{
+	public static String FileName = "InsecureBankv2.apk";
+	public static String builder(String FileName) throws IOException{
+	 ProcessBuilder builder = new ProcessBuilder(
+			  "cmd.exe", "/c", "cd \"C:\\dex2jar-2.0\" && d2j-dex2jar.bat ",FileName);
+	 builder.redirectErrorStream(true);
+	 Process p = builder.start();
+	 BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	  while (true) {
+		  String line;
+		  line = r.readLine();
+		  if (line == null) {break;}
+		  System.out.println(line);
+	  }
+	return null;
+	}
+	  public static void main(String[] args) throws IOException {
+		String Conversion2Class;
+		Conversion2Class = builder(FileName); 
+	  }
+}
