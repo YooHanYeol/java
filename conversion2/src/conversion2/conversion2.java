2478ae177818255f849522e2b40f7b0404d1f682
YooHanYeol <37393504+YooHanYeol@users.noreply.github.com>
2019-06-08 03:09:09 +0900

    dex파일 변환(jar 파일 생성)

---
+++ conversion2/src/conversion2/conversion2.java
@@ -0,0 +1,29 @@
+package conversion2;
+import java.io.*;
+class conversion2{
+	public static String FileName = "classes.dex";
+	public static String builder(String FileName){
+	try {
+	 ProcessBuilder builder = new ProcessBuilder(
+			  "cmd.exe", "/c", "cd \"C:\\dex2jar-2.0\" && d2j-dex2jar.bat ",FileName);
+	 builder.redirectErrorStream(true);
+	 Process p = builder.start();
+	 BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	 while (true) {
+		  String line;
+		  line = r.readLine();
+		  if (line == null) {break;}
+		  System.out.println(line);
+	  }
+	 }
+		catch (IOException e) {
+	        e.printStackTrace();
+	}
+	return null;
+	}
+	  public static void main(String[] args) {
+		String Conversion2Class;
+		Conversion2Class = builder(FileName); 
+		System.out.println("dex -> jar 파일변환 성공");
+	  }
+}
